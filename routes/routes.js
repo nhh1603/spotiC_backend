aa7712f81c25392388fca5c5ff02d8467ab3c180
nhh1603 <nghiahieu12@gmail.com>
2023-06-04 10:24:43 +0200

    authenticate users, add admin, update song and playlist schema

--- routes/routes.js
+++ routes/routes.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
-
 // import controllers
 const { getTest } = require('../controllers/test');
-const { submitSignUp }= require('../controllers/submitSignUp');
+const { signupUser, getUsers, loginUser, getUserById, updateUserById, deleteUserById }= require('../controllers/user');
 
 //import middlewares
-
+const auth = require('../middlewares/auth');
+const admin = require('../middlewares/admin');
+const validObjectId = require('../middlewares/validObjectId');
 
 // api routes
 router.get('/', getTest);
-router.post('/submitSignUp', submitSignUp);
+router.get('/user', admin, getUsers);
+router.get('/user:id', [validObjectId, auth], getUserById);
+router.put('/user:id', [validObjectId, auth], updateUserById);
+router.delete('/user:id', [validObjectId, auth], deleteUserById);
+// router.post('/submitSignUp', submitSignUp);
+router.post('/submitSignUp', signupUser);
+router.post('/login', loginUser);
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const cors = require('cors');
 require('dotenv').config();
+require("express-async-errors")
 
 // app
 const app = express();
