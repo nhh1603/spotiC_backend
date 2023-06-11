7891ac9707716fe14e85fb293114636f98837deb
nhh1603 <nghiahieu12@gmail.com>
2023-06-11 12:04:20 +0200

    minor modification

--- models/Song.js
+++ models/Song.js
@@ -3,17 +3,20 @@ const joi = require('joi');
 
 const songSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    // artist: { type: String, required: true },
-    artists: { type: [String], default: [], required: true },
-    album: { type: String, required: true },
+    artistId: { type: String, required: true },
+    // artistIds: { type: [String], default: [], required: true },
+    artistName: { type: String, required: true }, // need delete
+    albumId: { type: String, required: true },
     song: { type: String, required: true },
     cover: { type: String, required: true },
+    duration: { type: Number, required: true}, // need delete
     listenTimes: { type: Number, default: 0 },
 });
 
 const validate = (song) => {
     const schema = joi.object({
         name: joi.string().required(),
+        artistId: joi.string().required(),
         // artist: joi.string().required(),
         artists: joi.array().items(joi.string()).required(),
         album: joi.string().required(),
