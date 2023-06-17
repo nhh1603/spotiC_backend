1b347aea20304ef9d3681b98c40a9ca53e51ab39
nhh1603 <nghiahieu12@gmail.com>
2023-06-17 19:54:48 +0200

    fix bug

--- models/Playlist.js
+++ models/Playlist.js
@@ -5,17 +5,17 @@ const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const playlistSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    user: { type: ObjectId, ref: "user", required: true },
+    // user: { type: ObjectId, ref: "user", required: true },
     desc: { type: String },
     songs: { type: Array, default: [] },
     img: { type: String },
     collaborative: { type: Boolean, default: false },
 });
 
-const validate = () => {
+const validate = (playlist) => {
     const schema = joi.object({
         name: joi.string().required(),
-        user: joi.string().required(),
+        // user: joi.string().required(),
         desc: joi.string().allow(""),
         songs: joi.array().items(joi.string()),
         img: joi.string().allow(""),
